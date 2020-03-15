56f9d9c287e0eb6df7e8f0a5ec89e3d298136ca3
derekalangilbert <derekgilbert@outlook.com>
2020-03-15 14:06:32 -0600

    demo

--- v2/listings.js
+++ v2/listings.js
@@ -1,16 +1,41 @@
 const consola = require('consola')
-// const { HOA, BEDROOMS } = require('../types/listings/index')
+const { HOA, BEDROOMS, BATHROOMS, AREA, LOT, YEARS } = require('../types/listings/index')
 const ListingsQuery = require('./search.js')
 
+const hoaMonthlyFee = 300
+const bedroomsMin = 2
+const bedrooomsMax = 4
+const bathroomsMin = 2
+const bathroomsMax = 4
+const areaMin = 3000
+const areaMax = 5000
+const lotSizeMin = 400
+const lotSizeMax = 900
+const yearBuiltMin = 1989
+const yearBuiltMax = 2010
+const types = ['singleFamily', 'multiFamily']
+
 const query = ListingsQuery.create()
-  .where('bedrooms')
-  .isBetween(3, 4)
+  .where(BEDROOMS)
+  .isBetween(bedroomsMin, bedrooomsMax)
   .and()
-  .where('hoaMonthlyFee')
-  .isLessThan(400)
+  .where(BATHROOMS)
+  .isBetween(bathroomsMin, bathroomsMax)
   .and()
-  .hasHoa(400)
+  .where(YEARS)
+  .isBetween(yearBuiltMin, yearBuiltMax)
   .and()
-  .hasPropertyTypes(['singleFamily', 'multiFamily'])
+  .where(AREA)
+  .isBetween(areaMin, areaMax)
+  .and()
+  .where(LOT)
+  .isBetween(lotSizeMin, lotSizeMax)
+  .and()
+  .where(HOA)
+  .isLessThan(hoaMonthlyFee)
+  .and()
+  .hasHoa(hoaMonthlyFee)
+  .and()
+  .hasPropertyTypes(types)
 
 consola.success('listings-query:', query.string)
