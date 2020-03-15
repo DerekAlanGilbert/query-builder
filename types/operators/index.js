e7f7f2866438dbdf692fead2223c3bcc4951fca1
derekalangilbert <derekgilbert@outlook.com>
2020-03-15 14:36:04 -0600

    created logical operator types

---
+++ types/operators/index.js
@@ -0,0 +1,15 @@
+const greaterThan = '<'
+const lesserThan = '>'
+const greaterThanOrEqualTo = '<='
+const lesserThanOrEqualTo = '>='
+const equalTo = '='
+const notEqualTo = '!='
+
+module.exports = {
+  greaterThan,
+  lesserThan,
+  greaterThanOrEqualTo,
+  lesserThanOrEqualTo,
+  equalTo,
+  notEqualTo
+}
