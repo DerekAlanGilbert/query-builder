21fb37a890bfce3c9bad157bfbe5e5dcdec6be48
derekalangilbert <derekgilbert@outlook.com>
2020-03-15 22:57:02 -0600

    added constants and exported created

--- v2/search.js
+++ v2/search.js
@@ -1,15 +1,19 @@
+const { AND, OR } = require('../types/delimiters')
+const { equalTo } = require('../types/operators')
 const Query = require('./index.js')
 
 Query.prototype.hasPropertyTypes = function(properties) {
   if (properties.length === 0) return this
-  this.string += `(${properties.map(type => `propertyType = ${type}`).join(' or ')})`
+  this.string += `(${properties.map(type => `propertyType = ${type}`).join(` ${OR} `)})`
+  this.string += ` ${AND} `
   return this
 }
 
 Query.prototype.hasHoa = function(hoaMonthlyFee) {
   if (!hoaMonthlyFee) return this
-  this.string += 'hasHoa = "true"'
+  this.string += `hasHoa ${equalTo} "true"`
+  this.string += ` ${AND} `
   return this
 }
 
-module.exports = Query
+module.exports = Query.create()
