cf660b6e0b4ca65a8f09532ad53bf0e5074e5388
derekalangilbert <derekgilbert@outlook.com>
2020-03-15 22:57:26 -0600

    Removed and(), changes fields to be nullable

--- v2/listings.js
+++ v2/listings.js
@@ -2,40 +2,35 @@ const consola = require('consola')
 const { HOA, BEDROOMS, BATHROOMS, AREA, LOT, YEARS } = require('../types/listings')
 const ListingsQuery = require('./search.js')
 
+// #region --  data
 const hoaMonthlyFee = 300
 const bedroomsMin = 2
 const bedrooomsMax = 4
 const bathroomsMin = 2
-const bathroomsMax = 4
-const areaMin = 3000
-const areaMax = 5000
-const lotSizeMin = 400
-const lotSizeMax = 900
-const yearBuiltMin = 1989
-const yearBuiltMax = 2010
-const types = ['singleFamily', 'multiFamily']
+const bathroomsMax = null
+const areaMin = null
+const areaMax = null
+const lotSizeMin = null
+const lotSizeMax = null
+const yearBuiltMin = null
+const yearBuiltMax = null
+const types = ['singleFamily']
+// #endregion -- data
 
-const query = ListingsQuery.create()
-  .where(BEDROOMS)
+const filterString = ListingsQuery.where(BEDROOMS)
   .isBetween(bedroomsMin, bedrooomsMax)
-  .and()
   .where(BATHROOMS)
   .isBetween(bathroomsMin, bathroomsMax)
-  .and()
   .where(YEARS)
   .isBetween(yearBuiltMin, yearBuiltMax)
-  .and()
   .where(AREA)
   .isBetween(areaMin, areaMax)
-  .and()
   .where(LOT)
   .isBetween(lotSizeMin, lotSizeMax)
-  .and()
   .where(HOA)
   .isLessThan(hoaMonthlyFee)
-  .and()
   .hasHoa(hoaMonthlyFee)
-  .and()
   .hasPropertyTypes(types)
+  .build()
 
-consola.success('listings-query:', query.string)
+consola.success('GET listings-query:', filterString)
