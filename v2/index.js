effb5b74fe53c39121fe8202043e33691a4e3e7b
derekalangilbert <derekgilbert@outlook.com>
2020-03-15 22:55:50 -0600

    added and to every function

--- v2/index.js
+++ v2/index.js
@@ -1,12 +1,13 @@
-const { greaterThan, lesserThan } = require('../types/operators')
+// const { lastItemInString, removeItemFromString } = require('../utils')
+const { greaterThanOrEqualTo, lesserThanOrEqualTo } = require('../types/operators')
 const { OR, AND } = require('../types/delimiters')
 
 function Query(str = '') {
   this.string = str
 }
 
-Query.create = function(str) {
-  return new Query(str)
+Query.create = function() {
+  return new Query()
 }
 
 Query.prototype.where = function(str) {
@@ -15,10 +16,18 @@ Query.prototype.where = function(str) {
 }
 
 Query.prototype.isBetween = function(min, max) {
-  if (!min && !max) return this
   const lastItemInString = this.string.split(' ').pop()
   this.string = this.string.replace(lastItemInString, '')
-  this.string += `(${lastItemInString} ${greaterThan} ${min} ${AND} ${lastItemInString} ${lesserThan} ${max})`
+  if (!min && !max) {
+    return this
+  } else if (min && max) {
+    this.string += `${lastItemInString} ${greaterThanOrEqualTo} ${min} ${AND} ${lastItemInString} ${lesserThanOrEqualTo} ${max}`
+  } else if (min && !max) {
+    this.string += `${lastItemInString} ${greaterThanOrEqualTo} ${min}`
+  } else if (!min && max) {
+    this.string += `${lastItemInString} ${lesserThanOrEqualTo} ${max}`
+  }
+  this.string += ` ${AND} `
   return this
 }
 
@@ -26,7 +35,8 @@ Query.prototype.isLessThan = function(value) {
   if (!value) return this
   const lastItemInString = this.string.split(' ').pop()
   this.string = this.string.replace(lastItemInString, '')
-  this.string += `${lastItemInString} ${lesserThan} ${value}`
+  this.string += `${lastItemInString} ${lesserThanOrEqualTo} ${value}`
+  this.string += ` ${AND} `
   return this
 }
 
@@ -34,7 +44,8 @@ Query.prototype.isGreaterThan = function(value) {
   if (!value) return this
   const lastItemInString = this.string.split(' ').pop()
   this.string = this.string.replace(lastItemInString, '')
-  this.string += `${lastItemInString} ${lesserThan} ${value}`
+  this.string += `${lastItemInString} ${greaterThanOrEqualTo} ${value}`
+  this.string += ` ${AND} `
   return this
 }
 
@@ -46,6 +57,12 @@ Query.prototype.and = function() {
 Query.prototype.or = function() {
   this.string += ` ${OR} `
   return this
+}
+
+Query.prototype.build = function() {
+  // this.string = this.string.trim()
+  this.string = this.string.slice(0, -4)
+  return this.string
 }
 
 module.exports = Query
